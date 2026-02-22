257f87e3990fade33e10a0ee77a99070a7b4a9db
mmarronetravels-sudo <mmarronetravels@gmail.com>
2026-02-22 07:13:27 -0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1706,7 +1706,7 @@ if (studentData) {
       <svg className="absolute left-3 top-3.5 w-5 h-5 text-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
       </svg>
-    </div
+    </div>
     <div className="flex flex-wrap gap-2 mt-3">
       <select
         value={flagFilterAdmin}
